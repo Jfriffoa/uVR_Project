17064b9b5642b9d6aa82aef0df28931dba6a62da
José Francisco Riffo (U) <jfriffoa@gmail.com>
2020-01-07 18:54:03 +0100

    WIP -Rotate Map

---
+++ Assets/Scripts/RotateWorld.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Input;
+
+public class RotateWorld : MonoBehaviour
+{
+    public float degrees = 45f;
+    public float speed;
+
+    // Rotates Around Y Axis
+    public void Rotate(BaseInputEventData data)
+    {
+        Debug.Log("ROTATE", gameObject);
+        transform.Rotate(0, degrees, 0, Space.Self);
+    }
+}
